ce6b044cbfc0d947827b1330e8662c198304c149
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-06 03:45:54 +0700

    menggunakan assertions

--- src/main/java/programmer/zaman/now/test/Calculator.java
+++ src/main/java/programmer/zaman/now/test/Calculator.java
@@ -1,6 +1,15 @@
 package programmer.zaman.now.test;
 
 public class Calculator {
+
+    public Integer divide(Integer first, Integer second){
+        if(second == 0){
+            throw new IllegalArgumentException("Can't divided by zero");
+        }else {
+            return first / second;
+        }
+    }
+
     public Integer add(Integer first, Integer second){
         return first + second;
     }
