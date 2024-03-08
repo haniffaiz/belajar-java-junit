cf3e8b2887f658b084f3631da632e590ca0fb290
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-09 01:15:06 +0700

    siklus hidup test Lifecycle

--- src/test/java/programmer/zaman/now/test/OrderedTest.java
+++ src/test/java/programmer/zaman/now/test/OrderedTest.java
@@ -1,26 +1,48 @@
 package programmer.zaman.now.test;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+
+//test dijalankan per method
+//@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+
+//test dijalankan per class (sharing variable)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 
 //Mengurutkan test
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
+
+    private int counter = 0;
+
+    @BeforeAll
+    void beforeAll(){
+
+    }
+
+    @AfterAll
+    void afterAll(){
+
+    }
+
     @Test
     @Order(1)
     void test3() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     void test2() {
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     void test1() {
+        counter++;
+        System.out.println(counter);
     }
 }
