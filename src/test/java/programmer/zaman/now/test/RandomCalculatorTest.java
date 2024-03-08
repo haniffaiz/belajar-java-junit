4d7fe4cb671893a7a04f29e224b4479a7610fd6b
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-09 02:00:27 +0700

    test berulang

--- src/test/java/programmer/zaman/now/test/RandomCalculatorTest.java
+++ src/test/java/programmer/zaman/now/test/RandomCalculatorTest.java
@@ -1,7 +1,6 @@
 package programmer.zaman.now.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
 import programmer.zaman.now.test.resolver.RandomParameterResolver;
@@ -16,6 +15,39 @@ public class RandomCalculatorTest extends AbstractCalculatorTest{
 
     @Test
     void testRandom(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected,result);
+    }
+
+    @DisplayName("Test random calculator")
+    //perulangan test tiap methode
+    @RepeatedTest(
+            value = 10,
+            name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+    )
+    void testRandomRepeat(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+
+        var result = calculator.add(a,b);
+        var expected = a + b;
+
+        Assertions.assertEquals(expected,result);
+    }
+
+    @DisplayName("Test random calculator")
+    //perulangan test tiap methode
+    @RepeatedTest(
+            value = 10,
+            name = "{displayName}"
+    )
+    void testRandomRepeat(TestInfo info,Random random, RepetitionInfo repetitionInfo) {
+        System.out.println(info.getDisplayName() + " ke "+repetitionInfo.getCurrentRepetition()+" dari" + repetitionInfo.getTotalRepetitions());
         var a = random.nextInt();
         var b = random.nextInt();
 
